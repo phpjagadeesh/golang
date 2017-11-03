a42c03cbd31c0e7ac5692f44489bf12787c0b5d4
phpjagadeesh <p.jagadeesh11@gmail.com>
2017-11-03 12:20:19 +0530

    native language handle

--- helloworld.go
+++ helloworld.go
@@ -3,5 +3,6 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hello World!", "My First Program in GoLang");
+    fmt.Println("Hello World!", "My First Program in GoLang")
+    fmt.Println("GO nativly handle all languages!", "ജഗദീഷ്")
 }
