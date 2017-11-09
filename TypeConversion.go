27a0074ef20111e4fce85000173add046e303188
phpjagadeesh <p.jagadeesh11@gmail.com>
2017-11-09 13:53:49 +0530

    type conversion and constanr

--- TypeConversion.go
+++ TypeConversion.go
@@ -4,5 +4,13 @@ import "fmt"
 
 func main() {
     var x int = 1
-    fmt.Println(float64(x)) 
+    fmt.Println(float64(x))
+
+    // Type Interface
+    var y = 1
+    fmt.Println(y);
+
+    // Constant Value
+    const Pi = 3.14
+    fmt.Println(Pi)
 }
