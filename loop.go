862c7b4f75e3cda38a78a06eca0db60ec58b3b05
phpjagadeesh <p.jagadeesh11@gmail.com>
2017-11-09 14:11:52 +0530

    looping

---
+++ loop.go
@@ -0,0 +1,27 @@
+// Unlike other language no paranthesis needed
+
+package main 
+
+import "fmt"
+
+func main() {
+    for i:=1; i<10;
+    i++ {
+        fmt.Println("Incement ", i);
+    }
+
+    // init and post statement are option
+    // But conditional are manadetory
+    j := 1;      
+    for ; j<10 ; {
+      fmt.Println(j)
+      j  = j + 1 
+    }
+
+    // Bye Bye to 'while'
+    k := 10;
+    for k<=20 {
+        fmt.Println(k)
+        k++
+    }
+}
